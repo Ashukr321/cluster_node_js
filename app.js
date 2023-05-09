3449c43843d33a698927dbecd805b9f3908cf7a6
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-05-09 21:37:14 +0530

    cluster setup  is completed

---
+++ app.js
@@ -0,0 +1,15 @@
+// import the const express = require('express')
+const express = express();
+const app = express();
+
+const port = process.env.port || 5500;
+
+app.get("/", async (req, res) => {
+  const result = 0;
+  for (let i = 0; i < 1000000; i++) {
+    result += i;
+  }
+  return res.json({ processId: process.pid, result });
+});
+
+app.listen(port, () => console.log(`  listening on port ${port} and pid ${process.pid}`));
